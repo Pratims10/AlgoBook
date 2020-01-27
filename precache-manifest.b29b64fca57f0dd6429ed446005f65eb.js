a20a1227da285857a4f45c0ca28eaa3919d09405
Pratim Sarkar <pratimsarkar23@gmail.com>
2020-01-27 15:55:36 +0530

    Updates

---
+++ precache-manifest.b29b64fca57f0dd6429ed446005f65eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1039ef84abce4a82c1420678678b0681",
+    "url": "/AlgoBook/index.html"
+  },
+  {
+    "revision": "ed35db931f50e1b7dfe1",
+    "url": "/AlgoBook/static/css/main.d396fddf.chunk.css"
+  },
+  {
+    "revision": "be7eb3efe5a569e3b4c3",
+    "url": "/AlgoBook/static/js/2.702a4d96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AlgoBook/static/js/2.702a4d96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed35db931f50e1b7dfe1",
+    "url": "/AlgoBook/static/js/main.57e04fec.chunk.js"
+  },
+  {
+    "revision": "51fb421f1ef0b7caa029",
+    "url": "/AlgoBook/static/js/runtime-main.5b7a8584.js"
+  }
+]);
